94c557b431228bcff592c9099d3681e54c9a826b
PXgamer <owzie123@gmail.com>
2017-05-25 09:01:09 +0100

    Removed previous view files

--- searchbox.php
+++
@@ -1,6 +0,0 @@
-<div class="search-sect">
-    <form action="/search.php" method="post">
-        <input placeholder="I want to watch..." value="<?= isset($search) ? htmlspecialchars($search) : '' ?>"
-               autocomplete="off" name="q" type="text" class="hover-bottom big-search"/>
-    </form>
-</div>
