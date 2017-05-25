f081a4724b41dfb9ad5a4238ab6cf939f9864b45
PXgamer <owzie123@gmail.com>
2017-05-25 11:46:55 +0100

    Removed connection/info files

--- connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-$mysqli = new mysqli('localhost', 'root', 'root', 'yify-torrents');
-if ($mysqli->connect_error) {
-    die('Error : (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
-}
